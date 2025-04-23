07f7d903ab288aa55b3f67b1c64f9980a6d9eb9d
jiwonshin84 <jiwonshin84@gmail.com>
2025-04-24 08:48:21 +0900

    Update hoem.py

--- hoem.py
+++ hoem.py
@@ -4,3 +4,6 @@ st.set_page_config(page_title="홈", page_icon="🏠")
 
 st.title("🏠 홈 페이지")
 st.write("여기는 홈입니다. 좌측 사이드바에서 다른 페이지를 선택하세요.")
+
+st.sidebar.markdown("🏥 page2")
+st.sidebar.markdown("💸 page3")
