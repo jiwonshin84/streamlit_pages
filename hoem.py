a85985a21b2bb59666c8a11fb697f72c6e7a29d7
jiwonshin84 <jiwonshin84@gmail.com>
2025-04-24 08:50:28 +0900

    Update hoem.py

--- hoem.py
+++ hoem.py
@@ -8,10 +8,6 @@ st.write("여기는 홈입니다. 좌측 사이드바에서 다른 페이지를 
 st.sidebar.markdown("🏥 page2")
 st.sidebar.markdown("💸 page3")
 
-# Define the pages
-hoem = st.Page("home.py", title="Main Page", icon="🎈")
-page2 = st.Page("page2.py", title="Page 2", icon="❄️")
-page3 = st.Page("page3.py", title="Page 3", icon="🎉")
 
 
 # Set up navigation
